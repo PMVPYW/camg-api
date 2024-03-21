7f49ffa5a1a16162c8b2a2057776630f05ed41fd
Gameiro29 <miguelgameiro29@gmail.com>
2024-03-21 16:14:16 +0000

    Relations imagens_noticia->noticias->rally

--- app/Models/ImagemNoticia.php
+++ app/Models/ImagemNoticia.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ImagemNoticia extends Model
 {
     use HasFactory;
     protected $table = "imagens_noticia";
     protected $fillable = ["noticia_id", "image_id"];
+
+    public function noticia(): BelongsTo
+    {
+        return $this->belongsTo(Noticia::class);
+    }
 }
--- app/Models/Noticia.php
+++ app/Models/Noticia.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Noticia extends Model
 {
     use HasFactory;
     protected $table = "noticias";
     protected $fillable = ["rally_id", "titulo", "conteudo", "title_img", "data"];
+
+    public function rally(): BelongsTo {
+        return $this->belongsTo(Rally::class);
+    }
+
+    public function imagens(): HasMany{
+        return $this->hasMany(ImagemNoticia::class);
+    }
+
 }
