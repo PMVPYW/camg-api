b3e63cc19cea9da207fc82cedd0e00875bf0f799
Pedro Vicente <2210716@my.ipleiria.pt>
2024-03-23 15:51:15 +0000

    protected routes

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,35 @@ use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\RallyController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
+
+
+//unprotected routes
 Route::post('auth/login', [AuthenticationController::class, "login"])->name("login");
 
-    Route::apiResource("rally", RallyController::class);
-    Route::apiResource("album", AlbumController::class);
+Route::get("rally", [RallyController::class, "index"]);
+Route::get("rally/{rally}", [RallyController::class, "show"]);
+
+Route::get("album", [AlbumController::class, "index"]);
+Route::get("album/{album}", [AlbumController::class, "show"]);
+
+//protected routes
+Route::middleware("auth:sanctum")->group(function (){
+    Route::post("rally", [RallyController::class, "store"]);
+    Route::put("rally/{rally}", [RallyController::class, "update"]);
+    Route::delete("rally/{rally}", [RallyController::class, "destroy"]);
+
+   Route::post("album", [AlbumController::class, "store"]);
+   Route::put("album/{album}", [AlbumController::class, "update"]);
+   Route::delete("album/{album}", [AlbumController::class, "destroy"]);
+
+    Route::get('/user', function (Request $request) {
+        return Auth::user();
+    });
+});
