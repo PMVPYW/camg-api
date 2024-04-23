e0ab0f710cc92d019eed8092f5d0e81d849d311d
Pedro Vicente <2210716@my.ipleiria.pt>
2024-04-23 22:57:28 +0100

    create user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AdminRequest;
 use App\Http\Resources\AdminResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -20,9 +24,24 @@ class AdminController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(AdminRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $admin = null;
+        DB::transaction(function () use ($validated, &$admin, $request) {
+            $admin = new User();
+            if ($request->hasFile("photo_url")) {
+                $file = $request->file("photo_url");
+                $file_type = $file->getClientOriginalExtension();
+                $file_name_to_store = substr(base64_encode(microtime()), 3, 6) . '.' . $file_type;
+                Storage::disk('public')->put('fotos/' . $file_name_to_store, File::get($file));
+                $admin->photo_url = $file_name_to_store;
+            }
+            unset($validated["photo_url"]);
+            $admin->fill($validated);
+            $admin->save();
+        });
+        return new AdminResource($admin);
     }
 
     /**
