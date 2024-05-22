6fb74b9b75739c9bd811cc3020a55448625596a2
Gameiro29 <miguelgameiro29@gmail.com>
2024-05-22 12:17:09 +0100

    update messages contactos

--- app/Http/Requests/ContactoRequest.php
+++ app/Http/Requests/ContactoRequest.php
@@ -41,14 +41,14 @@ class ContactoRequest extends FormRequest
         return [
             'nome.required' => 'O campo nome é obrigatório.',
             'nome.string' => 'O campo nome deve ser uma string.',
-            'tipo_valor.required' => 'O campo tipo_valor é obrigatório.',
-            'tipo_valor.in' => 'O campo tipo_valor deve ser um dos seguintes valores: Email, Telemovel, Telefone, Fax, Facebook, Instagram, Twitter, PaginaWeb, WhatsApp, Morada, Coordenadas.',
+            'tipo_valor.required' => 'O campo tipo é obrigatório.',
+            'tipo_valor.in' => 'O campo tipo deve ser um dos seguintes valores: Email, Telemovel, Telefone, Fax, Facebook, Instagram, Twitter, PaginaWeb, WhatsApp, Morada, Coordenadas.',
             'valor.required' => 'O campo valor é obrigatório.',
             'valor.string' => 'O campo valor deve ser uma string.',
             'valor.max' => 'O campo valor não pode ter mais que 9 caracteres quando do tipo Telemóvel ou Telefone.',
-            'tipocontacto_id.required' => 'O campo tipocontacto_id é obrigatório.',
-            'tipocontacto_id.integer' => 'O campo tipocontacto_id deve ser um número inteiro.',
-            'tipocontacto_id.exists' => 'O tipocontacto_id fornecido não existe na tabela tipo_contacto.',
+            'tipocontacto_id.required' => 'O campo tipo de contacto é obrigatório.',
+            'tipocontacto_id.integer' => 'O campo tipo de contacto deve ser um número inteiro.',
+            'tipocontacto_id.exists' => 'O tipo de contacto fornecido não existe na tabela tipo_contacto.',
         ];
     }
 }
--- app/Http/Requests/ContactoRequestUpdate.php
+++ app/Http/Requests/ContactoRequestUpdate.php
@@ -28,4 +28,21 @@ class ContactoRequestUpdate extends FormRequest
             "tipocontacto_id" => "sometimes|integer|exists:tipo_contacto,id"
         ];
     }
+    public function withValidator($validator): void
+    {
+        $validator->sometimes('valor', 'max:9', function ($input) {
+            return in_array($input->tipo_valor, ['Telemovel', 'Telefone']);
+        });
+    }
+    public function messages(): array
+    {
+        return [
+            'nome.string' => 'O campo nome deve ser uma string.',
+            'tipo_valor.in' => 'O campo tipo deve ser um dos seguintes valores: Email, Telemovel, Telefone, Fax, Facebook, Instagram, Twitter, PaginaWeb, WhatsApp, Morada, Coordenadas.',
+            'valor.string' => 'O campo valor deve ser uma string.',
+            'valor.max' => 'O campo valor não pode ter mais que 9 caracteres quando do tipo Telemóvel ou Telefone.',
+            'tipocontacto_id.integer' => 'O campo tipo de contacto deve ser um número inteiro.',
+            'tipocontacto_id.exists' => 'O tipo de contacto fornecido não existe na tabela tipo_contacto.',
+        ];
+    }
 }
